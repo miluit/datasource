96226f4a6ed3f352005dc7214bec03a409aee91a
miluit <milu52it@163.com>
2021-06-17 17:03:40 +0800

    上传到dev分支

--- src/main/java/com/taotao/demo/utils/ResCode.java
+++ src/main/java/com/taotao/demo/utils/ResCode.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum ResCode {
-
+     /** */
     SUCCESS(20000,"操作成功"),
     ERROR(50000,"操作失败"),
     SYSTEM_ERROR(50001,"系统操作");
